0266c490a40818f4b96ab0178e032769ba20c702
“Huzaifa0608” <huzaifafarhan7890@gmail.com>
2023-05-27 20:26:56 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,55 @@
+
+var div2 = document.getElementById("div1")
+function circle(){
+    div2.style.backgroundColor = "red"
+    div2.style.width = "500px"
+    div2.style.height = "500px"
+    div2.style.borderRadius = "500px"
+    
+
+}
+function rectangle(){
+    div2.style.backgroundColor = "blue"
+    div2.style.width = "800px"
+    div2.style.height = "500px"
+    div2.style.borderRadius = "0px"
+
+}function square(){
+    div2.style.backgroundColor = "green"
+    div2.style.width = "500px"
+    div2.style.height = "500px"
+    div2.style.borderRadius = "0px"
+
+}
+document.getElementById('btn1').style.border = "none"
+document.getElementById('btn1').style.backgroundColor = "grey"
+document.getElementById('btn1').style.borderRadius = "50px"
+document.getElementById('btn1').style.padding = "20px"
+document.getElementById('btn1').style.margin = "10px"
+
+document.getElementById('btn2').style.border = "none"
+document.getElementById('btn2').style.backgroundColor = "grey"
+document.getElementById('btn2').style.borderRadius = "50px"
+document.getElementById('btn2').style.padding = "20px"
+document.getElementById('btn2').style.margin = "10px"
+
+document.getElementById('btn3').style.border = "none"
+document.getElementById('btn3').style.backgroundColor = "grey"
+document.getElementById('btn3').style.borderRadius = "50px"
+document.getElementById('btn3').style.padding = "20px"
+document.getElementById('btn3').style.margin = "10px"
+
+document.getElementById("main").style.textAlign = 'center';
+document.getElementById("div1").style. margin = "auto"
+    
+      
+  
+
+
+// // button{
+//     border: none;
+//     background-color: grey;
+//     border-radius: 50px;
+//     padding: 20px;
+//     margin: 10px;
+// }//
